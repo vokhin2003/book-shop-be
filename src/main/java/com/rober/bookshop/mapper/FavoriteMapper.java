092c998b0a11afb9f348ed17e67b1edc1cddd154
Nguyễn Minh Tiến <nguyentien7a8@gmail.com>
2025-08-05 21:56:14 +0700

    fix(favorite): change FavoriteResponseDTO from bookId to BookResponseDTO

--- src/main/java/com/rober/bookshop/mapper/FavoriteMapper.java
+++ src/main/java/com/rober/bookshop/mapper/FavoriteMapper.java
@@ -10,6 +10,5 @@ import org.mapstruct.Mapping;
 public interface FavoriteMapper {
 
     @Mapping(target = "userId", source = "user.id")
-    @Mapping(target = "bookId", source = "book.id")
     FavoriteResponseDTO toResponse(Favorite favorite);
 }
--- src/main/java/com/rober/bookshop/model/response/FavoriteResponseDTO.java
+++ src/main/java/com/rober/bookshop/model/response/FavoriteResponseDTO.java
@@ -9,5 +9,5 @@ import lombok.Setter;
 public class FavoriteResponseDTO {
     private Long id;
     private Long userId;
-    private Long bookId;
+    private BookResponseDTO book;
 }
